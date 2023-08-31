a262ae2bbc62443d748454cce57dc6ffbfd03c15
vanshbansal003 <104072778+vanshbansal003@users.noreply.github.com>
2023-08-31 19:49:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+
+    return {
+        restrict : 'E',
+        templateUrl : 'modules/skills/views/member.html',
+        controller : 'SkillsMemberController',
+        controllerAs : 'vm',
+        bindToController : true ,
+        scope : {
+            member : '='
+        }
+
+    };
+
+}
